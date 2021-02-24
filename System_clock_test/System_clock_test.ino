1fbc309a0ab2286cb1032e3d40844b1ab778c729
George Gonzalez <ggonza29@ucsc.edu>
2021-02-23 17:39:25 -0800

    Arduino clock function

---
+++ System_clock_test/System_clock_test.ino
@@ -0,0 +1,20 @@
+unsigned long timeClock = 0;
+unsigned long timeDifferent = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ 
+ timeClock = millis();
+ Serial.print(" System clock time: ");
+ Serial.println(timeClock);
+
+ timeDifferent = timeClock - millis();
+ Serial.print("The time different from the clock and tracker: ");
+ Serial.println(timeDifferent);
+
+ 
+}
